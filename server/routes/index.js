2293f564e015e990af631fe20f8cda9e5f6f8141
EXIT <exitudio@gmail.com>
2016-10-18 00:19:02 -0400

    Add username in the Header & move jade+ejs that is not used to temp

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,22 @@ var router = express.Router();
 var passport = require('passport');
 var mongoose = require('mongoose');
 /* GET home page. */
-router.get('/student',isAuthenticated, function(req, res, next) {
+router.get('/student', function(req, res, next) {
   res.sendfile('StudentPage.html');
 });
 router.get('/index', function(req, res, next) {
-  res.sendfile('index.html');
-  //res.render('login',{ message: "" });
+    var userName = "";
+    if( req.user && req.user.Email){
+        userName = req.user.Email.substr(0, 10);
+    }
+    res.render('index', { userName: userName});
+});
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+    //res.render('login',{ message: "" });
 });
 
 // process the login form
@@ -19,15 +29,14 @@ router.get('/index', function(req, res, next) {
 //  failureFlash : true // allow flash messages
 //}));
 router.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/student', // redirect to the secure profile section
-    failureRedirect : '/index', // redirect back to the signup page if there is an error
+    successRedirect : '/index', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 }));
 
 //logout
 router.get('/logout', function(req, res, next) {
-  res.sendfile('index.html');
-  //res.render('login',{ message: "" });
+    res.render('index', { title: 'Express' });
 });
 
 
