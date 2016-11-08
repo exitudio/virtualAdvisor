3dc5464291b63123cb7323cdc80a9a970a26be30
parikhdhruvil007 <parikh.dhruvil007@gmail.com>
2016-11-08 18:20:51 -0500

    Course Advisor
    Concentrations for specific major and level

--- server/routes/api/programs.js
+++ server/routes/api/programs.js
@@ -19,4 +19,40 @@ router.post('/courses', function(req, res, next)
     })
 });
 
+router.post('/courseAdvisor', function (req, res, next)
+            {
+    var level = req.body.Level;
+    var title = req.body.Title;
+    mongoose.connection.db.collection('Programs', function(err, items){
+        items.find({"Level":level,"Title":title}).toArray(function(err, results){
+            res.send(results);
+        })
+    })
+    
+});
+
+router.post('/courseTest', function (req, res, next)
+            {
+    var name = req.body.name;
+    mongoose.connection.db.collection('People', function(err, items){
+        items.find({"name":name}).toArray(function(err, results){
+            if (results !== null)
+                {
+                    console.log("Concentration Test");
+                    var level = req.body.Level;
+                    var title = req.body.Title;
+                    mongoose.connection.db.collection('Programs', function(err, items){
+                        items.find({"Level":"Undergraduate","Title":"Computer Engineering"}).toArray(function(err, results){
+                            res.send(results[0].Concentrations);
+                        })
+    })
+                }
+            else{
+                console.log("Username does not exist in database");
+            }
+        })
+    })
+    
+});
+
 module.exports = router;
